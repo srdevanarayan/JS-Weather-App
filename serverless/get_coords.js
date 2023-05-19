5c00ccd0562c3997d39c911fdbcf15089a77087b
Devanarayan <devaspandanangal@gmail.com>
2023-05-19 11:48:30 +0530

    Downgraded to node fetch 2.6.1

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -1,5 +1,5 @@
-//const fetch = require("node-fetch");
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
+
 const { WEATHER_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
